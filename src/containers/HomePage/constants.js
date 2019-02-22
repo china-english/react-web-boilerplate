cb745021abfaddd8f24ea149f8c8725779e66057
zhangtong <1263695353@qq.com>
2019-02-22 17:47:43 +0800

    home page suit

---
+++ src/containers/HomePage/constants.js
@@ -0,0 +1,15 @@
+import catGif from './assets/cat.gif';
+import doItGif from './assets/doIt.gif';
+
+export const ITEM_GROUP = [
+  {
+    title: '基于 react 的 web 框架',
+    describe: 'react & redux & redux-form',
+    image: catGif,
+  },
+  {
+    title: '版本号: 0.0.1',
+    describe: '',
+    image: doItGif,
+  },
+];
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
-import catGif from './assets/cat.gif';
-import doItGif from './assets/doIt.gif';
+import { ITEM_GROUP } from './constants';
 import frameImage from './assets/frame.png';
 import './styles.scss';
 
-const HomePage = () => (
-  <div className="home-page">
-    {window.setHelmet({ title: 'home' })}
-    <div className="img-group">
+class HomePage extends React.Component {
+  renderItem = (item) => (
+    <div className="item-content">
       <img src={frameImage} alt="frameImage" className="frame-image" />
-      <img src={frameImage} alt="frameImage" className="frame-image" />
-    </div>
-    <div className="img-mask">
-      <img src={catGif} alt="catGif" className="gif-img" />
-      <img src={doItGif} alt="doItGif" className="gif-img" />
-    </div>
-    <div className="img-mask">
+      <img src={item.image} alt="catGif" className="gif-img" />
       <div className="div-mask">
-        <p>基于 react 的 web 框架 <em><br /> react & redux & redux-form</em></p>
-      </div>
-      <div className="div-mask">
-        <p>版本号:<em> 0.0.1</em></p>
+        <p>{item.title}<em><br /> {item.describe}</em></p>
       </div>
     </div>
-  </div>
-);
+  )
+  render() {
+    return (
+      <div className="home-page">
+        {window.setHelmet({ title: 'home' })}
+        <div className="image-group">
+          { ITEM_GROUP.map((item) => this.renderItem(item)) }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default HomePage;
